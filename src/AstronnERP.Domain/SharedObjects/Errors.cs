08e071b82583b4f42ca1459dd14c0a0d9b05c81e
Denis <d.steph.dev@gmail.com>
2026-07-27 00:02:07 +0400

    feat: add custom errors for result pattern and value object for strings which should not be empty

---
+++ src/AstronnERP.Domain/SharedObjects/Errors.cs
@@ -0,0 +1,22 @@
+﻿using FluentResults;
+
+namespace AstronnERP.Domain.SharedObjects.Errors
+{
+    public class PropertyIsEmpty : Error
+    {
+        public PropertyIsEmpty(string propertyName)
+            : base($"{propertyName} property must contain something.")
+        {
+            Metadata.Add("ErrorCode", "COMMON_PROPERTY_EMPTY");
+        }
+    }
+
+    public class PropertyValueIsTheSame: Error
+    {
+        public PropertyValueIsTheSame(string propertyName)
+            : base($"New {propertyName} is the same as old.")
+        {
+            Metadata.Add("ErrorCode", "COMMON_PROPERTY_SAME");
+        }
+    }
+}
