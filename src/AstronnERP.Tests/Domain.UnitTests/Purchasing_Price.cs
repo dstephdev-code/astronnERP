5c34a087bc3e12b7a82bced556983ff3d06f1104
Denis <d.steph.dev@gmail.com>
2026-07-26 20:49:52 +0400

    tests: add price result testing

---
+++ src/AstronnERP.Tests/Domain.UnitTests/Purchasing_Price.cs
@@ -0,0 +1,53 @@
+﻿using AstronnERP.Domain.SharedObjects.ValueObjects;
+using AstronnERP.Domain.SharedObjects.Enums;
+
+namespace AstronnERP.Domain.UnitTests;
+
+public class Purchasing_Price
+{
+    [Fact]
+    public void Price_WhenValueBelowZero_ShouldReturnIsFailedTrue()
+    {
+        // Assign and Act
+        var result = Price.Create(-10, Currency.USD);
+
+        // Assert
+        Assert.IsNotType<Price>(result);
+        Assert.True(result.IsFailed);
+        Assert.Single(result.Errors);
+        Assert.Equal("Price must be greater than zero.", result.Errors[0].Message);
+    }
+
+    [Fact]
+    public void Price_WhenCurrencyIsNotInEnum_ShouldReturnIsFailedTrue()
+    {
+        var result = Price.Create(10, (Currency)5);
+
+        Assert.IsNotType<Price>(result);
+        Assert.True(result.IsFailed);
+        Assert.Single(result.Errors);
+        Assert.Equal("Currency must be of expected list.", result.Errors[0].Message);
+    }
+
+    [Fact]
+    public void Price_WhenBothValueAndCurrencyWrong_ShouldReturnTwoErrors()
+    {
+        var result = Price.Create(-10, (Currency)5);
+
+        Assert.IsNotType<Price>(result);
+        Assert.True(result.IsFailed);
+        Assert.Equal(2, result.Errors.Count);
+        Assert.Equal("Price must be greater than zero.", result.Errors[0].Message);
+        Assert.Equal("Currency must be of expected list.", result.Errors[1].Message);
+    }
+
+    [Fact]
+    public void Price_WhenBothValueAndCurrencyValid_ShouldReturnPrice()
+    {
+        var result = Price.Create(10, Currency.USD);
+
+        Assert.IsType<Price>(result.ValueOrDefault);
+        Assert.Equal(10, result.Value.Value);
+        Assert.Equal(Currency.USD, result.Value.Currency);
+    }
+}
