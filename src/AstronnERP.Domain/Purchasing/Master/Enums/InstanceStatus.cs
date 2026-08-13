5bdf4e5bfd8cfd916f24370dba91fb0915b2e95c
Denis <d.steph.dev@gmail.com>
2026-08-13 13:50:16 +0400

    feat: add domain methods to entities; remove component instance status

--- src/AstronnERP.Domain/Purchasing/Master/Enums/InstanceStatus.cs
+++ src/AstronnERP.Domain/Purchasing/Master/Enums/InstanceStatus.cs
@@ -5,7 +5,6 @@
         InReceiving,
         InStock,
         Issued,
-        Component,
         ReturnedToVendor
     }
 }
--- src/AstronnERP.Domain/Purchasing/Master/Warehouse.cs
+++ src/AstronnERP.Domain/Purchasing/Master/Warehouse.cs
@@ -1,4 +1,6 @@
-﻿using AstronnERP.Domain.SharedObjects.ValueObjects;
+﻿using AstronnERP.Domain.SharedObjects.Errors;
+using AstronnERP.Domain.SharedObjects.ValueObjects;
+using FluentResults;
 
 namespace AstronnERP.Domain.Purchasing.Master
 {
@@ -8,5 +10,37 @@ namespace AstronnERP.Domain.Purchasing.Master
         public Guid Id { get; init; }
 
         public NonEmptyString Name { get; private set; }
+
+        private Warehouse(NonEmptyString name)
+        {
+            Id = Guid.CreateVersion7();
+            Name = name;
+        }
+        
+        public static Result<Warehouse> CreateNew(string name)
+        {
+            var nameValidationResult = NonEmptyString.Create(name, nameof(Name));
+
+            if (nameValidationResult.IsFailed)
+                return nameValidationResult.ToResult();
+
+            return Result.Ok(new Warehouse(nameValidationResult.Value));
+        }
+
+        public Result ChangeName(string newName)
+        {
+            var newNameValidationResult = NonEmptyString.Create(newName, nameof(Name));
+            var isSameValue = newNameValidationResult.IsSuccess && string.Equals(newNameValidationResult.Value.Value, Name.Value);
+
+            var failureCheck = Result.Merge(
+                newNameValidationResult,
+                Result.FailIf(isSameValue, new PropertyValueIsTheSame(nameof(Name)))
+            );
+
+            if (failureCheck.IsSuccess)
+                Name = newNameValidationResult.Value;
+
+            return failureCheck.ToResult();
+        }
     }
 }
